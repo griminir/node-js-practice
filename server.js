caf344e1d2bed13b6ac8f2d652e148db5b327b3d
griminir <viktor.elliot.king@gmail.com>
2024-09-03 09:41:03 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const sql = require('mssql');
+const cors = require('cors');
+const config = require('./dbconfig');
+
+const app = express();
+app.use(cors());
+
+app.get('/api/todo', async (req, res) => {
+  const conn = await sql.connect(config);
+  const data = await conn.request().query('select * from ToDoItems');
+  const todo = res.json(data.recordset);
+  return todo;
+});
+
+app.get('/api/todo/:id', async (req, res) => {
+  const id = parseInt(req.params.id);
+  const conn = await sql.connect(config);
+  const data = await conn
+    .request()
+    .input('id', sql.Int, id)
+    .query(`select * from ToDoItems where id = @id`);
+  if (data.recordset.length === 0) {
+    return res.status(404).send('sorry that id does not exsist for tasks');
+  }
+  res.json(data.recordset);
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}...`);
+});
